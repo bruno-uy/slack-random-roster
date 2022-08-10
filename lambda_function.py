2104814919fa8feb48dfa4a33098c6ab5ddd1cf5
bruno-hl <bruno@homelight.com>
2022-08-10 11:48:45 -0300

    Added rich_text formatter in get_slack_body

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 import random
 from datetime import datetime
 from os import getenv
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 import urllib3
 
@@ -25,29 +25,58 @@ def render_roster(chosen: str, roster: List[str], render_chosen: bool) -> str:
     """
     roster = [f"\t{name}\n" for name in roster]
 
-    output = (
-        "Roster:\n"
-        f"{''.join(roster)}"
-    )
+    output = f"Roster:\n{''.join(roster)}"
     if render_chosen:
         output = f"Chosen: {chosen}\n\n{output}"
     return output
 
 
-def get_slack_body(body_text):
-    return {"text": body_text}
-
-
-def get_rocket_body(body_text):
+def get_slack_body(chosen: str, roster: List[str], render_chosen: bool):
+    elements = []
+    for member in roster:
+        member_element = {
+            "type": "rich_text_section",
+            "elements": [{"type": "text", "text": member}],
+        }
+        if render_chosen and member == chosen:
+            member_element["elements"][0]["text"] = f"{member} "
+            member_element["elements"].append({"type": "emoji", "name": "star"})
+        elements.append(member_element)
     return {
-        "username": "slack-random-roster",
-        "icon_emoji": ":robot:",
-        "text": body_text,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "<!here>, this is the order for the standup:",
+                },
+            },
+            {
+                "type": "rich_text",
+                "block_id": "roster",
+                "elements": [
+                    {
+                        "type": "rich_text_list",
+                        "elements": elements,
+                        "style": "bullet",
+                        "indent": 0,
+                    }
+                ],
+            },
+        ]
     }
 
 
-def get_ms_teams_body(body_text):
-    return {"text": body_text}
+def get_rocket_body(chosen: str, roster: List[str], render_chosen: bool) -> Dict:
+    return {
+        "username": "slack-random-roster",
+        "icon_emoji": ":robot:",
+        "text": render_roster(chosen, roster, render_chosen),
+    }
+
+
+def get_ms_teams_body(chosen: str, roster: List[str], render_chosen: bool) -> Dict:
+    return {"text": render_roster(chosen, roster, render_chosen)}
 
 
 def post(webhook, body):
@@ -72,10 +101,9 @@ def post(webhook, body):
 
     except Exception as e:
         exit_code["statusCode"] = 500
-        exit_code["body"] = (
-            f"Other Exception: {str(e)}"
-            f" (input argument of --url_hook : {webhook})"
-        )
+        exit_code[
+            "body"
+        ] = f"Other Exception: {str(e)} (input argument of --url_hook : {webhook})"
 
     return exit_code
 
@@ -87,11 +115,14 @@ def lambda_handler(event, context):
     }
 
     members = getenv("TEAM_ROSTER", default=None)
-    render_chosen = getenv("RENDER_CHOSEN", default=False)
+    render_chosen = getenv("RENDER_CHOSEN", default="False").lower() == "true"
     renders = [
         {"url": getenv("SLACK_WEBHOOK_URL", default=None), "render": get_slack_body},
         {"url": getenv("ROCKET_WEBHOOK_URL", default=None), "render": get_rocket_body},
-        {"url": getenv("MS_TEAMS_WEBHOOK_URL", default=None), "render": get_ms_teams_body},
+        {
+            "url": getenv("MS_TEAMS_WEBHOOK_URL", default=None),
+            "render": get_ms_teams_body,
+        },
     ]
 
     if members:
@@ -100,11 +131,10 @@ def lambda_handler(event, context):
         return exit_code
 
     chosen, roster = generate_roster(members)
-    roster_output = render_roster(chosen, roster, render_chosen)
 
     for render in renders:
         if render["url"]:
-            body = render["render"](roster_output)
+            body = render["render"](chosen, roster, render_chosen)
             exit_code = post(render["url"], body)
 
     return exit_code
